f70a2541150c5219eae6c5060e925e985256ac38
egorlitin <egorulitin@gmail.com>
2021-10-31 22:21:01 +0300

    fix

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,6 @@
-export default (name) => {
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
